8671ab2e3d4f17272f7cbc9926aaade03ba31037
Simón Yáñez <syanez@maquintel.com>
2025-08-07 19:20:12 -0400

    SY: Learning C++ to optimize

---
+++ engine/CMakeFiles/engine.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/engine.dir/src/main.cpp.o: \
+ /home/simao/Repositories/Cloud-Compression/engine/src/main.cpp \
+ /usr/include/stdc-predef.h
---
+++ engine/src/Main.cpp
@@ -0,0 +1,8 @@
+// src/main.cpp
+# include <iostream>
+
+int main() {
+	std::cout << "First C++ Attempt" << std::endl;
+	std::cin.get();
+}
+
